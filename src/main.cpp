83854c9942b218fc8646e7f154fcaadbe57d78c8
Oliver Westermann <oliver.westermann@cognex.com>
2023-07-18 10:33:30 +0200

    Add basic connectivity to MPU6050

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 #define PIN_DIR   4
 #define PIN_STEP  16
@@ -11,6 +12,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(PIN_DIR, OUTPUT);
   pinMode(PIN_STEP, OUTPUT);
+  Wire.begin();
 }
 
 void print_step_stuff(unsigned int steps, unsigned int direction) {
@@ -37,6 +39,28 @@ void rotate(unsigned int steps, unsigned int direction) {
   }
 }
 
+#define MPU6050_ADDR        0x68
+#define MPU6050_REG_WHOAMI  0x75
+
+uint8_t read_byte(uint8_t chip, uint8_t reg, uint8_t *buffer) {;
+  uint8_t error;
+  Wire.beginTransmission(chip);
+  Wire.write(reg);
+  error = Wire.endTransmission();
+  if (error) {
+    Serial.print("Error in ");
+    Serial.print(__func__);
+    Serial.println(error);
+  }
+  Wire.requestFrom(chip, (uint8_t)1);
+
+  // Slave may send less than requested
+  if (Wire.available()) {
+      *buffer = Wire.read();    // Receive a byte as character
+  }
+  return error;
+}
+
 void loop() {
   // put your main code here, to run repeatedly
   int dir = 1;
@@ -46,4 +70,8 @@ void loop() {
   delay(1000);
   rotate(steps, LEFT);
   delay(1000);
+  uint8_t whoami;
+  read_byte(MPU6050_ADDR, MPU6050_REG_WHOAMI, &whoami);
+  Serial.println(whoami, HEX);
+  whoami = 0;
 }
